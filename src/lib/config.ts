7c4a154ce656c0c281333b99e64f62b60458d59e
Nick Hodges <nickhodges@gmail.com>
2025-03-09 22:29:01 -0400

    Added Pagination

---
+++ src/lib/config.ts
@@ -0,0 +1,28 @@
+/**
+ * Application configuration settings
+ */
+export const config = {
+  /**
+   * Pagination settings
+   */
+  pagination: {
+    /**
+     * Number of sayings to display per page
+     */
+    sayingsPerPage: 6,
+  },
+  
+  /**
+   * Site metadata
+   */
+  site: {
+    /**
+     * Site title
+     */
+    title: "Two Kinds of People",
+    /**
+     * Site description
+     */
+    description: "Exploring humanity's endless dualities, one saying at a time",
+  },
+};
